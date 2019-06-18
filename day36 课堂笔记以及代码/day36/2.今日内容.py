a9a368f55c73149b459df0e12539544872b6ee12
太白金星 <412495691@qq.com>
2019-06-18 12:46:15 +0800

    day36

---
+++ day36 课堂笔记以及代码/day36/2.今日内容.py
@@ -0,0 +1,14 @@
+# 池 concurrent.futrues
+    # 线程池    *****
+    # 进程池
+# 协程
+    # 概念  *****
+    # gevent  第三方模块  ****
+        # 会用
+        # 能处理一些基础的网络操作
+    # asyncio 内置模块 py3.4 底层的协程模块
+        # aiohttp模块 并发的爬虫
+        # flask轻量级的web框架/sanic异步的轻量级的web框架
+        # 了解两个关键字
+            # async   *****
+            # await   *****
